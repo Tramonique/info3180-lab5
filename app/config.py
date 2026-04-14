4cea5a2e853d8d24b8fec15977d48fa3be666d36
Tramonique <tramoniquewellington@gmail.com>
2026-04-13 21:29:53 -0500

    Create movie model and database migration

--- app/config.py
+++ app/config.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env if it exists.
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    UPLOAD_FOLDER =  os.path.join(basedir, "uploads")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
